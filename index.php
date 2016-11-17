14e6f71beb1d3e70e0d1e4c3c1d297cb1570db9a
kim curran <currankim7@gmail.com>
2016-11-17 14:59:42 -0800

    create initial file structure and successfully run with xampp

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php include("inc/header.php"); ?>
+
+    <div class="section catalog random">
+
+    <div class="wrapper">
+
+      <h2>May we suggest something?</h2>
+
+      <ul class="catalog">
+        <li>
+          <a href="#">
+            <img src="" alt="">
+            <p>View Details</p>
+          </a>	
+        </li>
+        <li>
+          <a href="#">
+            <img src="" alt="">
+            <p>View Details</p>
+          </a>	
+        </li>
+        <li>
+          <a href="#">
+            <img src="" alt="">
+            <p>View Details</p>
+          </a>	
+        </li>
+      </ul>
+
+    </div>
+
+    </div>
+  </div>
+
+<?php include("inc/footer.php"); ?>
