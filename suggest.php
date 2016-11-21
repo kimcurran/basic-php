7c4af493d70a1d3340625cb3f1664a7ea765cd9a
kim curran <currankim7@gmail.com>
2016-11-21 14:17:45 -0800

    add and style suggest form

--- suggest.php
+++ suggest.php
@@ -4,6 +4,10 @@ include("inc/header.php"); ?>
 
 <div class="section page">
   <h1>Suggest a Media Item</h1>
+  <form method="post">
+    <label for="name">Name:</label><input type="text" name="name" id="name" />
+    <label for="email">Email Address:</label><input type="text" name="email" id="email" />
+    <label for="suggestion">Suggestion:</label><textarea name="suggestion" id="suggestion"></textarea>
+    <button type="submit">Submit</button>
+  </form>
 </div>
-
-<?php include("inc/footer.php"); ?>
