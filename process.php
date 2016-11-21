9c64183e9673ec4e6eb62037b86805831137f07a
kim curran <currankim7@gmail.com>
2016-11-21 14:26:55 -0800

    set up form to handle data

---
+++ process.php
@@ -0,0 +1,9 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$suggestion = $_POST["suggestion"];
+
+echo $name;
+echo $email;
+echo $suggestion;
+?>
--- suggest.php
+++ suggest.php
@@ -4,7 +4,7 @@ include("inc/header.php"); ?>
 
 <div class="section page">
   <h1>Suggest a Media Item</h1>
-  <form method="post">
+  <form method="post" action="process.php">
     <label for="name">Name:</label><input type="text" name="name" id="name" />
     <label for="email">Email Address:</label><input type="text" name="email" id="email" />
     <label for="suggestion">Suggestion:</label><textarea name="suggestion" id="suggestion"></textarea>
