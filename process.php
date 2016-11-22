8a496fe27b170d81b5f9685f30422d767e8e50e5
kim curran <currankim7@gmail.com>
2016-11-21 16:04:35 -0800

    fix form handling, remove extraneous file

--- process.php
+++
@@ -1,9 +0,0 @@
-<?php
-$name = $_POST["name"];
-$email = $_POST["email"];
-$suggestion = $_POST["suggestion"];
-
-echo $name;
-echo $email;
-echo $suggestion;
-?>
--- suggest.php
+++ suggest.php
@@ -1,13 +1,27 @@
 <?php 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $suggestion = $_POST["suggestion"];
+
+  //TODO send email
+  header("location:suggest.php?status=thanks");
+}
+
 $pageTitle = "Suggest a Media Item";
 include("inc/header.php"); ?>
 
 <div class="section page">
   <h1>Suggest a Media Item</h1>
-  <form method="post" action="process.php">
+  <form method="post" action="suggest.php" id="suggest-form">
+    <?php if (isset($_GET["status"]) && $_GET["status"] == "thanks") {
+    	echo "<p>Thanks for the suggestion! We'll check it shortly.</p>";
+    } else { ?>
     <label for="name">Name:</label><input type="text" name="name" id="name" />
     <label for="email">Email Address:</label><input type="text" name="email" id="email" />
     <label for="suggestion">Suggestion:</label><textarea name="suggestion" id="suggestion"></textarea>
     <button type="submit">Submit</button>
   </form>
+  <?php } ?>
 </div>
